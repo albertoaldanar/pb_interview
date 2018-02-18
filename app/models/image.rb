1017b97f270936b08023bb8eda864751e5365bad
Alberto Aldana <albertoaldanar@hotmail.com>
2018-02-17 23:39:59 -0700

    Create card with image when user creates an instance of Image

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,4 @@
 class Image < ApplicationRecord
-   has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }
+   has_attached_file :photo, styles: { small: "50x50>", thumb: "100x100>" }, default_url: "https://cloud.netlifyusercontent.com/assets/344dbf88-fdf9-42bb-adb4-46f01eedd629/68dd54ca-60cf-4ef7-898b-26d7cbe48ec7/10-dithering-opt.jpg"
    validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
 end
