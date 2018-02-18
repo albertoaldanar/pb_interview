efd0e8f33b43f2eed58e8289a02a2f79edde2da2
Alberto Aldana <albertoaldanar@hotmail.com>
2018-02-17 17:30:27 -0700

    Basic first design ready

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,2 @@
 class Image < ApplicationRecord
-  attr_accessible :title, :file
-  has_attached_file :file, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
 end
