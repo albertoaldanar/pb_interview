e6441046c38d3ed3764ef963d413ce51d4965b7a
Alberto Aldana <albertoaldanar@hotmail.com>
2018-02-18 14:57:51 -0700

    Delete options all set

--- app/assets/javascripts/options.js
+++ app/assets/javascripts/options.js
@@ -1,6 +1,14 @@
 //Hover para mostrar las opciones en cada foto
 $(document).ready(function(){
   $(".my_image").hover(function(){
-      $(".options").toggleClass("hidden");
+    $(".options").toggleClass("hidden");
+  });
+
+  $(".delete-btn").click(function(){
+    $(".options-image").removeClass("hidden");
+  });
+
+  $(".exit-btn").click(function(){
+    $(".options-image").addClass("hidden");
   });
 });
