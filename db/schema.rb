89ad13265f304bca8676eb1f8555f12978f2cc6b
Alberto Aldana <albertoaldanar@hotmail.com>
2018-02-17 18:08:33 -0700

    Added attachment photo atribute to image

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180216185341) do
+ActiveRecord::Schema.define(version: 20180218010659) do
 
   create_table "images", force: :cascade do |t|
     t.string   "title"
     t.string   "url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
 end
